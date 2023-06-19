539b40b937a2326c20008010e72dc191f0b96e1b
dummyx <40601688+dummyx@users.noreply.github.com>
2023-06-20 01:48:54 +0900

    Add settings example

---
+++ arduino/include/settings-example.h
@@ -0,0 +1,14 @@
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define SERVER_ADDRESS ""
+#define SMARTPHONE_IP remote_ip(0, 0, 0, 0)
+
+#define SET_ON_URL "https://HOST/set_on?secret=SECRET"
+#define SET_OFF_URL "https://HOST/set_off?secret=SECRET"
+#define GET_COMMAND_URL "https://HOST/get_command"
+#define UPLOAD_DATA_URL "https://HOST/upload_data?secret=SECRET&light=%d&temperature=%.2f&humidity=%.2f&pressure=%.2f"
+
+#define IR_SEND_PIN 17
+#define LIGHT_READ_PIN 36
+
+#define LIGHT_THRESHOLD 2300;
